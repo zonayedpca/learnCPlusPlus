d7cad40f18762357b187e3ed29468a9cf5985675
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-11-06 16:43:57 +0530

    Commenting

---
+++ 03. Comments/README.md
@@ -0,0 +1,16 @@
+How to use comment in the program
+
+## Single Line Comment
+```
+// Your Comment Here
+...code
+
+...code // Your Comment Here
+```
+
+## Multiline Comment
+```
+/*
+    Your Comments Here
+*/
+```
---
+++ 03. Comments/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+// must have main() 
+int main() {
+    /*
+        Multiline
+        Comment
+    */
+    std::cout << "Comments!" << std::endl; // Single Line Comment
+    // Single Line Comment
+    return 0;
+}
