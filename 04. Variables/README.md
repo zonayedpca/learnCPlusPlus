76b6bddfca9c3662dfd9dbe0a1224c4249696b48
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-11-06 16:51:10 +0530

    variables

---
+++ 04. Variables/README.md
@@ -0,0 +1,16 @@
+Intro to Variable
+
+## Declare a variable 
+```cpp
+int x;
+```
+
+## Assign value
+```cpp
+x = 5;
+```
+
+## Can be at once
+```cpp
+int y = 10;
+```
---
+++ 04. Variables/main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int main() {
+    int x = 10;
+    int y = 20;
+    std::cout << "x is: " << x << " and y is: " << y << std::endl;
+    return 0; 
+}
